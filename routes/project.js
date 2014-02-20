98c65de496da9db6334e5b5d0688a3bb51ef4bbc
cloudy991 <cloudy991@gmail.com>
2014-02-20 18:23:08 +0000

    database lab

--- routes/project.js
+++ routes/project.js
@@ -1,6 +1,6 @@
 var models = require('../models');
 
-exports.projectInfo = function(req, res) { 
+exports.projectInfo = function(req, res) {
   var projectID = req.params.id;
 
   // query for the specific project and
@@ -10,6 +10,8 @@ exports.projectInfo = function(req, res) { 
     if(err) console.log(err);
     res.json(projects[0]);
   }
+
+  models.Project.find({"_id": projectID}).exec(afterQuery);
 }
 
 exports.addProject = function(req, res) {
@@ -18,6 +20,28 @@ exports.addProject = function(req, res) {
 
   // make a new Project and save it to the DB
   // YOU MUST send an OK response w/ res.send();
+
+    var newData = new models.Project({
+      "title": form_data.project_title,
+      "date": form_data.date,
+      "summary": form_data.summary,
+      "image": form_data.image_url
+    });
+
+  function saveCallback(err) {
+    if(err) { 
+      console.log(err); 
+    }
+    else {
+      console.log("No Error during save!");
+      res.send();
+    }
+  }
+
+    newData.save(saveCallback);
+
+
+
 }
 
 exports.deleteProject = function(req, res) {
@@ -25,4 +49,17 @@ exports.deleteProject = function(req, res) {
 
   // find the project and remove it
   // YOU MUST send an OK response w/ res.send();
+
+  function deleteCallback(err) {
+    if(err) { 
+      console.log(err); 
+    }
+    else {
+      console.log("No Error during remove!");
+      res.send();
+    }
+  }
+
+  models.Project.find({"_id": projectID}).remove().exec(deleteCallback);
+
 }
